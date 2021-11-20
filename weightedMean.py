30bdc899f73790097f7971dea8b11cddb807e6e2
Libin Mathew Varghese <libin.varghese@fullcontact.com>
2021-11-20 10:57:08 +0530

    Update weightedMean.py

--- weightedMean.py
+++ weightedMean.py
@@ -6,13 +6,7 @@ import random
 import re
 import sys
 
-#
-# Complete the 'weightedMean' function below.
-#
-# The function accepts following parameters:
-#  1. INTEGER_ARRAY X
-#  2. INTEGER_ARRAY W
-#
+
 
 def weightedMean(X, W):
     # Write your code here
