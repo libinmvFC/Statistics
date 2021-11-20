9c3d32c376fb75bd279a2bf44772a7da5e472e97
Libin Mathew Varghese <libin.varghese@fullcontact.com>
2021-11-20 10:51:47 +0530

    Initial commit

---
+++ weightedMean.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'weightedMean' function below.
+#
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY X
+#  2. INTEGER_ARRAY W
+#
+
+def weightedMean(X, W):
+    # Write your code here
+    prod = 0
+    wSum = 0
+    for i in range(len(X)):
+        prod += X[i]*W[i]
+        wSum += W[i]
+    print(round((prod/wSum),1))
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    vals = list(map(int, input().rstrip().split()))
+
+    weights = list(map(int, input().rstrip().split()))
+
+    weightedMean(vals, weights)
